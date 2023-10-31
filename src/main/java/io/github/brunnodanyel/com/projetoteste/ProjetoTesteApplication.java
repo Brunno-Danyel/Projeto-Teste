6f5ed21ef491fbff8a253faac6ce12ba203a3bb8
Brunno-Danyel <brunnodanyel25rbr@hotmail.com>
2023-10-31 20:36:19 -0300

    Criação do projeto

---
+++ src/main/java/io/github/brunnodanyel/com/projetoteste/ProjetoTesteApplication.java
@@ -0,0 +1,13 @@
+package io.github.brunnodanyel.com.projetoteste;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoTesteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoTesteApplication.class, args);
+	}
+
+}
