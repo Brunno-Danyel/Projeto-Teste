7e318bb3adc4db5af9594f6fe44bc0ccde1db388
Brunno-Danyel <brunnodanyel25rbr@hotmail.com>
2023-10-31 20:43:08 -0300

    Implementando a classe de usuário

---
+++ src/main/java/io/github/brunnodanyel/com/projetoteste/model/User.java
@@ -0,0 +1,27 @@
+package io.github.brunnodanyel.com.projetoteste.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Generated;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nome;
+
+    @Column(unique = true)
+    private String email;
+
+    private String senha;
+
+}
